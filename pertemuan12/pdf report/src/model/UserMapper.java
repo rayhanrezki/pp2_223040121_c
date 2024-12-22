232215d6cfa4b37c4baa0085eb87994bb47ca596
Rayhan Alfa Rezki <125686480+rayhanrezki@users.noreply.github.com>
2024-12-22 22:56:28 +0700

    pertemuan 12

---
+++ pertemuan12/pdf report/src/model/UserMapper.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package model;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface UserMapper {
+     @Select("SELECT * FROM users")
+    List<User> getAllUsers();
+    
+    @Insert("INSERT INTO users (name, email) VALUES (#{name}, #{email})")
+    void insertUser(User user);
+}
